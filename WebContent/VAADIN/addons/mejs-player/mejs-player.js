035d46b9b91b3781b7b194c9b4791ac412cf3a6c
Bryson Dunn <k.bryson.dunn@gmail.com>
2016-03-09 18:18:03 -0700

    Add Vimeo Support and Fix Flash/Silverlight Fallback

--- WebContent/VAADIN/addons/mejs-player/mejs-player.js
+++ WebContent/VAADIN/addons/mejs-player/mejs-player.js
@@ -1,8 +1,21 @@
 var mejsplayer = mejsplayer || {};
 
+/* Add plugin MIME types */
+mejs.plugins.silverlight[0].types.push("wmv", "video/x-ms-wmv");
+mejs.plugins.silverlight[0].types.push("avi", "video/x-msvideo");
+mejs.plugins.silverlight[0].types.push("wma", "audio/x-ms-wma");
+mejs.plugins.silverlight[0].types.push("wav", "audio/x-wav");
+mejs.plugins.silverlight[0].types.push("mp3", "audio/mpeg");
+mejs.plugins.silverlight[0].types.push("aac", "audio/x-aac");
+mejs.plugins.flash[0].types.push("mp4", "video/mp4");
+mejs.plugins.flash[0].types.push("flv", "video/x-flv");
+mejs.plugins.flash[0].types.push("mp3", "audio/mpeg");
+mejs.plugins.flash[0].types.push("aac", "audio/x-aac");
+
 mejsplayer.player = function (element, mejsOptions, mcOptions, rpcOptions) {
     
     var t = this;
+    var p = 'VAADIN/addons/mejs-player/mediaelement-2.20.0/';
     var d = mejsOptions.enablePluginDebug === true;
     
     /* Create the HTML5 audio or video element */
@@ -34,15 +47,15 @@ mejsplayer.player = function (element, mejsOptions, mcOptions, rpcOptions) {
     }
     
     function addFallback() { 
-        var flashFallback = 
-            '<object type="application/x-shockwave-flash" data="media-element/flashmediaelement.swf">' +
-                '<param name="movie" value="media-element/flashmediaelement.swf" />' +
-                '<param name="flashvars" value="controls=true&file=nothing" />' +
+        var flashFallback = //width="100%" height="100%" 
+            '<object type="application/x-shockwave-flash" data="' + p + 'flashmediaelement.swf">' +
+                '<param name="movie" value="' + p + 'flashmediaelement.swf" />' +
+                '<param name="flashvars" value="controls=true&amp;file=nothing" />' +
             '</object>';
         var silverlightFallback = 
-            '<object type="application/x-silverlight-2" data="media-element/silverlightmediaelement.xap">' +
-                '<param name="movie" value="media-element/silverlightmediaelement.xap" />' +
-                '<param name="flashvars" value="controls=true&file=nothing" />' +
+            '<object type="application/x-silverlight-2" data="' + p + 'silverlightmediaelement.xap">' +
+                '<param name="movie" value="' + p + 'silverlightmediaelement.xap" />' +
+                '<param name="flashvars" value="controls=true&amp;file=nothing" />' +
             '</object>';
         
         /* Enable Flash and/or Silverlight fallback */
@@ -51,16 +64,23 @@ mejsplayer.player = function (element, mejsOptions, mcOptions, rpcOptions) {
             $('#' + mcOptions.uid).append(flashFallback);
             $('#' + mcOptions.uid).append(silverlightFallback);
             mejsOptions.plugins = ['flash', 'silverlight'];
+            mejsOptions.pluginPath = p;
+            mejsOptions.flashName = 'flashmediaelement.swf';
+            mejsOptions.silverlightName = 'silverlightmediaelement.xap';
             
         } else if (mcOptions.flash == true) {
             if (d) console.log('Adding Flash fallback');
             $('#' + mcOptions.uid).append(flashFallback);
             mejsOptions.plugins = ['flash'];
+            mejsOptions.pluginPath = p;
+            mejsOptions.flashName = 'flashmediaelement.swf';
             
         } else if (mcOptions.silverlight == true) {
             if (d) console.log('Adding Silverlight fallback');
             $('#' + mcOptions.uid).append(silverlightFallback);
             mejsOptions.plugins = ['silverlight'];
+            mejsOptions.pluginPath = p;
+            mejsOptions.silverlightName = 'silverlightmediaelement.xap';
         }
     }
     
@@ -76,6 +96,16 @@ mejsplayer.player = function (element, mejsOptions, mcOptions, rpcOptions) {
         if (rpcOptions.seekedRpc) { mediaElement.addEventListener('seeked', seeked, false); }
         if (rpcOptions.volumeChangeRpc) { mediaElement.addEventListener('volumechange', volumechange, false); }
         if (rpcOptions.loadedDataRpc) { mediaElement.addEventListener('loadeddata', loadeddata, false); }
+        
+        if (d) console.log("MediaElement.js player created");
+    };
+    
+    mejsOptions.error = function () {
+    	if (d) console.log("Error creating MediaElement.js player!")
+    	var e = $('#' + mcOptions.uid).siblings('.me-cannotplay');
+    	e.css('text-align', 'center');
+    	e.children('a').css('font-weight', 'normal').css('display', 'block').css('height', '100%');
+    	e.find('span').css('line-height', '200%').css('padding', '0').text('Unsupported Format - Download File');
     }
     
     /* Create MEJS player */
@@ -87,33 +117,56 @@ mejsplayer.player = function (element, mejsOptions, mcOptions, rpcOptions) {
         mejsplayer.pause();
         
         var el = $('#' + mcOptions.uid);
-        var yt = source.type === 'video/youtube';// || source.type === "video/vimeo";
-        var tp = (source.type.indexOf("video") != -1 || yt) ? 'video' : 'audio';
-        var oyt = $('#' + mcOptions.uid).attr('type') === 'video/youtube';
+        var s = source.src;
+        var t = source.type;
+        var tc = $('#' + mcOptions.uid).attr('type') !== t;
+        var pt = (t.indexOf("video") != -1 || yt || vm) ? 'video' : 'audio';
+        var yt = t === 'video/youtube';// || $('#' + mcOptions.uid).attr('type') === 'video/youtube';
+        var vm = t === 'video/vimeo';// || $('#' + mcOptions.uid).attr('type') === 'video/vimeo';
         
-        /* Re-create the MEJS Player if the type has changed or if YouTube */
-        if (mcOptions.playerType !== tp || yt || oyt) {
+        /* Re-create the MEJS Player if the type has changed or if YouTube/Vimeo */
+        if (tc || yt || vm) {
             if (d) console.log('Replacing MediaElementPlayer');
-            mcOptions.playerType = tp;
+            mcOptions.playerType = pt;
             el.first().get(0).player.remove();
-            initializeDOM(!yt); // Don't add fallback if YouTube
+            initializeDOM(!yt && !vm); // Don't add fallback if YouTube/Vimeo
             el = $('#' + mcOptions.uid);
-            el.attr('type', source.type).attr('src', source.src);
-            if (yt) {
+            el.attr('src', s).attr('type', t);
+            el.children('source').attr('src', s).attr('type', t);
+            $('#' + mcOptions.uid + ' object param[name="flashvars"]').attr('value', 'controls=true&amp;file=' + s);
+            if (yt || vm) {
                 mejsOptions.plugins.reverse();
-                mejsOptions.plugins.push('youtube');
+                mejsOptions.plugins.push(yt ? 'youtube' : 'vimeo');
                 mejsOptions.plugins.reverse();
                 if (d) console.log('Enabled plugins: ' + mejsOptions.plugins);
+            } else {
+            	for(var i = mejsOptions.plugins.length - 1; i >= 0; i--) {
+            	    if(mejsOptions.plugins[i] === 'youtube' || mejsOptions.plugins[i] === 'vimeo') {
+            	       array.splice(i, 1);
+            	    }
+            	}
             }
             mejsplayer = new MediaElementPlayer(el, mejsOptions);
         } 
-        /* Set the source if not YouTube */
-        if (!yt) {
-            if (d) console.log('Setting mejs-player #' + mcOptions.uid + ' source to ' + source.src + ' [' + source.type + ']');
-            mejsplayer.setSrc(source.src);
-            $('#' + mcOptions.uid + ' object param[name="flashvars"]').attr('value', 'controls=true&amp;file=' + source.src);
+        /* Set the source if not YouTube/Vimeo */
+        if (!yt && !vm) {
+            if (d) console.log('Setting mejs-player #' + mcOptions.uid + ' source to ' + s + ' [' + t + ']');
+            try {
+            	mejsplayer.setSrc(s);
+            } catch (err) {
+            	// This should only happen if using a plugin (flash/silverlight)
+            	if (d) console.log('Error setting source. The Flash or Silverlight plugin is in use.');
+            	el.parent().parent().parent().trigger('click');
+            }
         }
         mejsplayer.load();
+        
+        /* Vimeo fix */
+        if (vm) {
+        	el.parent().siblings('.mejs-controls').css('display', 'none');
+        	el.siblings('.me-plugin').css('height', '100%').css('width', '100%')
+        		.children('.mejs-shim').css('height', '100%').css('width', '100%');
+        }
     }
     
     /* Get player state */
@@ -167,8 +220,7 @@ mejsplayer.player = function (element, mejsOptions, mcOptions, rpcOptions) {
         t.notifyPlaying();
     }
     function pause () {
-        //if (d) 
-            console.log('Client notification: pause');
+        if (d) console.log('Client notification: pause');
         t.notifyPaused();
     }
     function play () {
